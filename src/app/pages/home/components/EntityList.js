082e5190013e8a58a7448a72ac33cbc8d2af7481
Sebastian Delmont <sd@notso.net>
2022-10-24 11:28:27 -0400

    [marathon] Follow exact order from DXMarathon submission spreadsheet

--- src/app/pages/home/components/EntityList.js
+++ src/app/pages/home/components/EntityList.js
@@ -14,10 +14,30 @@ import { useSelector } from "react-redux"
 import { selectEntrySelections } from "../../../store/entries"
 import { EntityEntry } from "./EntityEntry"
 
+// DX Marathon form has some entities out of order
+// * CE0X after CE0Z
+// * FO after FO/c
+// * FR after FT/j
+// * FS after FT/t
+// * HK0/a after HK0/m
+// * VP8/h after VP8/s
+const REORDERED_PREFIXES = {
+  CE0X: "CE0Zzzz",
+  FO: "FO/czzz",
+  FR: "FT/jzzz",
+  FS: "FT/tzzz",
+  "HK0/a": "HK0/mzzz",
+  "VP8/h": "VP8/szzz",
+}
+
 const CQWWEntities = Object.values(ENTITIES).sort((a, b) => {
   const attr = "entityPrefix"
-  const aValue = a[attr][0] === "*" ? a[attr].slice(1) : a[attr]
-  const bValue = b[attr][0] === "*" ? b[attr].slice(1) : b[attr]
+  let aValue = a[attr][0] === "*" ? a[attr].slice(1) : a[attr]
+  let bValue = b[attr][0] === "*" ? b[attr].slice(1) : b[attr]
+
+  aValue = REORDERED_PREFIXES[aValue] || aValue
+  bValue = REORDERED_PREFIXES[bValue] || bValue
+
   return aValue.localeCompare(bValue)
 })
 
