7471a2173a507b52de15ef0d0723902a3e8d31d6
Sebastian Delmont <sd@notso.net>
2022-11-12 09:47:31 -0500

    [marathon] fixed react warning

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react"
-import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
 import { BrowserRouter } from "react-router-dom"
+import { createRoot } from "react-dom/client"
 
 import App from "./app/app"
 import { persistor, store } from "./app/store"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = createRoot(document.getElementById("root"))
 root.render(
   <StrictMode>
     <Provider store={store}>
