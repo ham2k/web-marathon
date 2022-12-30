7e2da7d52bcef65f4f0c463c9343517f6a3a0cd6
Sebastian Delmont <sd@notso.net>
2022-12-30 06:32:42 -0500

    [marathon] use ClubLog APIs [WIP]

---
+++ src/app/store/clublog/clublogSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  loading: false,
+  error: undefined,
+  lastLoaded: undefined,
+  cty: {},
+}
+
+export const clublogSlice = createSlice({
+  name: "clublog",
+
+  initialState,
+
+  reducers: {
+    setClublogLoading: (state, action) => {
+      state.cty = {}
+      state.error = undefined
+      state.loading = action.payload
+      state.lastLoaded = undefined
+    },
+    setClublogError: (state, action) => {
+      state.cty = {}
+      state.error = action.payload
+      state.loading = false
+      state.lastLoaded = undefined
+    },
+    setClublogCtyData: (state, action) => {
+      state.cty = action.payload
+      state.error = undefined
+      state.loading = false
+      state.lastLoaded = new Date().valueOf()
+    },
+  },
+})
+
+export const { setClublogLoading, setClublogCtyData } = clublogSlice.actions
+
+export const fetchClublogCtyData = () => (dispatch) => {
+  dispatch(setClublogLoading(true))
+  dispatch(setClublogLoading(false))
+}
+
+export default clublogSlice.reducer
