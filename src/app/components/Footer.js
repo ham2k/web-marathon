7c50b2bc6e0e298cc23a6face65917173b0aa36f
Sebastian Delmont <sd@notso.net>
2023-11-26 18:11:18 -0500

    Footer links

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -11,12 +11,11 @@ export function Footer ({ styles }) {
       <span title={`${packageJson.version} ${window.currentEnv} ${window.currentCommit.substr(0, 7)}`}>
         Ham2k <b>Marathon Tools</b>
       </span>
-      &nbsp;&nbsp;•&nbsp;&nbsp; Developed by <a href='https://www.qrz.com/db/KI2D'>KI2D</a> - Sebastian Delmont{' '}
-      <a href='https://twitter.com/sd'>@sd</a>
+      &nbsp;&nbsp;•&nbsp;&nbsp; Developed by <a href='https://www.qrz.com/db/KI2D'>KI2D</a> - <a href='https://sdelmont.com'>Sebastián Delmont</a>{' '}
       &nbsp;&nbsp;•&nbsp;&nbsp;
       <GitHub fontSize='small' sx={{ verticalAlign: 'baseline', position: 'relative', top: '4px' }} />
       &nbsp;
-      <a href='https://github.com/ham2k/ham2k'>github.com/ham2k</a>
+      <a href='https://github.com/ham2k'>github.com/ham2k</a>
     </Box>
   )
 }
